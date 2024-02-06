450009de1f586fbf9a638b85e20adf7853d0f3cf
lw453 <30123882+lw453@users.noreply.github.com>
2024-02-06 01:33:12 -0500

    Output genes by O/E vs. rCNV score discordance with robust Z

--- scripts/constraint/compare_gene_oe_vs_rcnv_ds.R
+++ scripts/constraint/compare_gene_oe_vs_rcnv_ds.R
@@ -9,6 +9,7 @@
 # Contact: Ryan L. Collins <rlcollins@g.harvard.edu>
 
 # Plot gene DEL and DUP observed/expected values (O/Es) by rCNV pHaplo or pTriplo bin
+# and write out genes ranked by discordance of O/Es vs. rCNV scores
 
 
 #########
@@ -48,19 +49,25 @@ load.gene.scores <- function(del.tsv, dup.tsv, rcnv.tsv) {
     # Compute expected # of dels & dups given sample size
     gene.scores$exp.DEL <- (10^gene.scores$mu.DEL) * 2 * n.samples
     gene.scores$exp.DUP <- (10^gene.scores$mu.DUP) * 2 * n.samples
+
+    # Compute obs/exp
+    gene.scores$oe.DEL <- gene.scores$n_svs.DEL / gene.scores$exp.DEL
+    gene.scores$oe.DUP <- gene.scores$n_svs.DUP / gene.scores$exp.DUP
     return(gene.scores)
 }
 
 # Compute gene O/E summary statistics with confidence intervals per rCNV score bin
-compute.oe.by.rcnv.bin <- function(gene.scores, n.bootstraps = 1000, seed = 42) {
-    oe <- as.data.frame(do.call("cbind", lapply(c("DEL", "DUP"), function(cnv) {
-        t(sapply(1:100, function(i) {
+compute.oe.stats.by.rcnv.bin <- function(gene.scores, n.bootstraps = 1000, seed = 42) {
+    oe.stats <- do.call("cbind", lapply(c("DEL", "DUP"), function(cnv) {
+        do.call("rbind", lapply(1:100, function(i) {
             idxs <- which(gene.scores[, paste(cnv, "pct", sep = ".")] == i)
             obs <- gene.scores[idxs, paste("n_svs", cnv, sep = ".")]
             exp <- gene.scores[idxs, paste("exp", cnv, sep = ".")]
+
             # Bootstrap sampling of genes
             set.seed(seed)
             bootstrap.idxs <- replicate(n.bootstraps, sample(idxs, length(idxs), replace = TRUE))
+
             # Get obs and exp per gene in bootstrap samples
             bootstrap.obs <- matrix(
                 gene.scores[bootstrap.idxs, paste("n_svs", cnv, sep = ".")],
@@ -70,6 +77,7 @@ compute.oe.by.rcnv.bin <- function(gene.scores, n.bootstraps = 1000, seed = 42) 
                 gene.scores[bootstrap.idxs, paste("exp", cnv, sep = ".")],
                 ncol = n.bootstraps
             )
+
             # Compute confidence intervals on bin summary statistics
             bootstrap.medians <- colMedians(bootstrap.obs / bootstrap.exp)
             median.ci <- quantile(bootstrap.medians, c(0.05, 0.95))
@@ -80,24 +88,68 @@ compute.oe.by.rcnv.bin <- function(gene.scores, n.bootstraps = 1000, seed = 42) 
                 sum(obs) / sum(exp), sums.ci[1], sums.ci[2]
             )
         }))
-    })))
-    colnames(oe) <- c(
+    }))
+    colnames(oe.stats) <- c(
         "median.DEL", "median.DEL.lower", "median.DEL.upper",
         "sum.DEL", "sum.DEL.lower", "sum.DEL.upper",
         "median.DUP", "median.DUP.lower", "median.DUP.upper",
         "sum.DUP", "sum.DUP.lower", "sum.DUP.upper"
     )
-    return(oe)
+    return(oe.stats)
 }
 
 # Compute correlation coefficients and p-values for each relationship
-compute.cor <- function(oe) {
-    cors <- as.data.frame(do.call("rbind", lapply(seq_along(oe), function(i) {
-        cor.vals <- cor.test(seq_along(oe[, i]), unlist(oe[, i]), method = "spearman")
-        data.frame(names(oe)[i], cor.vals$estimate, cor.vals$p.value)
-    })))
+compute.cor <- function(oe.stats) {
+    cors <- do.call("rbind", lapply(seq_along(oe.stats), function(i) {
+        cor.vals <- cor.test(
+            seq_along(oe.stats[, i]), unlist(oe.stats[, i]),
+            method = "spearman"
+        )
+        data.frame(names(oe.stats)[i], cor.vals$estimate, cor.vals$p.value)
+    }))
     colnames(cors) <- c("sum.type", "estimate", "p")
     return(cors)
+}
+
+# Compute robust Z-scores of O/Es for genes within each rCNV score bin
+compute.z.by.rcnv.bin <- function(gene.scores) {
+    zs.by.cnv <- lapply(c("DEL", "DUP"), function(cnv) {
+        # Calculate median and MAD O/Es per bin
+        bin.stats <- cbind(pct = 1:100, do.call("rbind", lapply(
+            1:100,
+            function(i) {
+                oes <- gene.scores[
+                    gene.scores[, paste(cnv, "pct", sep = ".")] == i,
+                    paste("oe", cnv, sep = ".")
+                ]
+                data.frame(bin.median.oe = median(oes), bin.mad.oe = mad(oes))
+            }
+        )))
+        colnames(bin.stats) <- paste(cnv, colnames(bin.stats), sep = ".")
+
+        # Annotate genes with bin-level median and MAD O/E
+        scores <- merge(
+            gene.scores[, c(
+                "gene",
+                paste(cnv, "pct", sep = "."),
+                paste("n_svs", cnv, sep = "."),
+                paste("exp", cnv, sep = "."),
+                paste("oe", cnv, sep = ".")
+            )], bin.stats,
+            by = paste(cnv, "pct", sep = ".")
+        )
+
+        # Calculate gene robust Z-scores
+        scores <- cbind(
+            scores,
+            (scores[, paste("oe", cnv, sep = ".")] -
+                scores[, paste(cnv, "bin.median.oe", sep = ".")]) /
+                scores[, paste(cnv, "bin.mad.oe", sep = ".")]
+        )
+        colnames(scores)[length(colnames(scores))] <- paste(cnv, "bin.z", sep = ".")
+        return(scores)
+    })
+    return(merge(zs.by.cnv[[1]], zs.by.cnv[[2]], by = "gene"))
 }
 
 
@@ -108,7 +160,7 @@ compute.cor <- function(oe) {
 plot.rcnv.bin.oe <- function(dat, cors) {
     par(mfrow = c(2, 2), mar = c(2, 3.5, 2, 1.5))
     plot(
-        as.numeric(dat$sum.DEL),
+        dat$sum.DEL,
         pch = 19, col = DEL.colors$main,
         xaxt = "n", xlab = "", ylab = "", las = 2, main = "DEL Obs/Exp (Bin-wide Mean)",
         panel.first = c(abline(h = 1, lty = 5))
@@ -126,8 +178,9 @@ plot.rcnv.bin.oe <- function(dat, cors) {
     )
     mtext(1, text = "Genes Binned by pHaplo Percentile")
     mtext(2, line = 2.5, text = "Observed / Expected")
+
     plot(
-        as.numeric(dat$sum.DUP),
+        dat$sum.DUP,
         pch = 19, col = DUP.colors$main,
         xaxt = "n", xlab = "", ylab = "", las = 2, main = "DUP Obs/Exp (Bin-wide Mean)",
         panel.first = c(abline(h = 1, lty = 5))
@@ -145,8 +198,9 @@ plot.rcnv.bin.oe <- function(dat, cors) {
     )
     mtext(1, text = "Genes Binned by pTriplo Percentile")
     mtext(2, line = 2.5, text = "Observed / Expected")
+
     plot(
-        as.numeric(dat$median.DEL),
+        dat$median.DEL,
         pch = 21, col = DEL.colors$main,
         xaxt = "n", xlab = "", ylab = "", las = 2, main = "DEL Obs/Exp (Median Gene)",
         panel.first = c(abline(h = 1, lty = 5))
@@ -164,8 +218,9 @@ plot.rcnv.bin.oe <- function(dat, cors) {
     )
     mtext(1, text = "Genes Binned by pHaplo Percentile")
     mtext(2, line = 2.5, text = "Observed / Expected")
+
     plot(
-        as.numeric(dat$median.DUP),
+        dat$median.DUP,
         pch = 21, col = DUP.colors$main,
         xaxt = "n", xlab = "", ylab = "", las = 2, main = "DUP Obs/Exp (Median Gene)",
         panel.first = c(abline(h = 1, lty = 5))
@@ -196,7 +251,7 @@ option_list <- list()
 # Get command-line arguments & options
 args <- parse_args(
     OptionParser(
-        usage = "%prog del.tsv dup.tsv rcnv.tsv out.pdf",
+        usage = "%prog del.tsv dup.tsv rcnv.tsv out.pdf out_genes_del.tsv out_genes_dup.tsv",
         option_list = option_list
     ),
     positional_arguments = TRUE
@@ -204,8 +259,13 @@ args <- parse_args(
 opts <- args$options
 
 # Checks for appropriate positional arguments
-if (length(args$args) != 4) {
-    stop(paste("Four positional arguments required: del.tsv, dup.tsv, rcnv.tsv, and out.pdf"))
+if (length(args$args) != 6) {
+    stop(
+        paste(
+            "Six positional arguments required:",
+            "del.tsv, dup.tsv, rcnv.tsv, out.pdf, out_genes_del.tsv, and out_genes_dup.tsv"
+        )
+    )
 }
 
 # Writes args & opts to vars
@@ -213,17 +273,40 @@ del.tsv <- args$args[1]
 dup.tsv <- args$args[2]
 rcnv.tsv <- args$args[3]
 out.pdf <- args$args[4]
+out_genes_del.tsv <- args$args[5]
+out_genes_dup.tsv <- args$args[6]
 
-# Load gene scores
+# Load gene obs, mus, exps, and rCNV scores
 gene.scores <- load.gene.scores(del.tsv, dup.tsv, rcnv.tsv)
 
-# Compute gene O/E summary statistics per rCNV bin
-oe <- compute.oe.by.rcnv.bin(gene.scores)
+# # Compute gene O/E summary statistics per rCNV score bin
+# oe.stats <- compute.oe.stats.by.rcnv.bin(gene.scores)
 
-# Compute correlations for each O/E summary statistic type with rCNV score percentile
-cors <- compute.cor(oe)
+# # Compute correlations for each O/E summary statistic type with rCNV score percentile
+# cors <- compute.cor(oe.stats)
 
-# Plot O/Es of genes by their rCNV score bin
-pdf(out.pdf, height = 5, width = 10)
-plot.rcnv.bin.oe(oe, cors)
-dev.off()
+# # Plot O/Es of genes by their rCNV score bin
+# pdf(out.pdf, height = 5, width = 10)
+# plot.rcnv.bin.oe(oe.stats, cors)
+# dev.off()
+
+# Compute robust Z-scores for O/Es of genes within rCNV score bins
+zs <- compute.z.by.rcnv.bin(gene.scores)
+
+# Output genes ranked by robust Z-score
+write.table(
+    zs[
+        order(abs(zs$DEL.bin.z), decreasing = TRUE),
+        c("gene", colnames(zs)[grepl("DEL", colnames(zs))])
+    ],
+    out_genes_del.tsv,
+    row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE
+)
+write.table(
+    zs[
+        order(abs(zs$DUP.bin.z), decreasing = TRUE),
+        c("gene", colnames(zs)[grepl("DUP", colnames(zs))])
+    ],
+    out_genes_dup.tsv,
+    row.names = FALSE, col.names = TRUE, sep = "\t", quote = FALSE
+)
