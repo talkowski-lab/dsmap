4a2c8e5b26d52e86fb93efbae680b57e8f5cd686
lw453 <30123882+lw453@users.noreply.github.com>
2024-05-09 15:08:44 -0400

    Change gene constraint plots to run once per constraint metric

--- scripts/constraint/compare_gene_constraint.R
+++ scripts/constraint/compare_gene_constraint.R
@@ -8,12 +8,8 @@
 # Distributed under terms of the MIT License (see LICENSE)
 # Contact: Ryan L. Collins <rlcollins@g.harvard.edu>
 
-# Plot gene DEL and DUP DSMap observed/expected values (O/Es) against constraint metrics:
-# 1. rCNV pHaplo or pTriplo,
-# 2. LOEUF,
-# 3. minimum missense constraint region O/E,
-# 4. v4 exome gene O/Es.
-# Write out genes ranked by discordance of DSMap O/Es vs. other constraint metrics
+# 1. Plot gene DEL and DUP DSMap observed/expected values (O/Es) against a constraint metric
+# 2. Write out genes ranked by discordance of DSMap O/Es vs. other constraint metrics
 
 
 #########
@@ -33,26 +29,47 @@ dsmapR::load.constants(c("colors"))
 # Data functions #
 ##################
 # Load data
-load.gene.scores <- function(del.tsv, dup.tsv, rcnv.tsv, n.bins = 100) {
+load.gene.scores <- function(del.tsv, dup.tsv, constraint.tsv, n.bins = 100) {
     # Load data files:
-    # gene observed & expected del & dup counts
-    # gene rCNV pHaplo & pTriplo scores
+    # gene del & dup observed & expected counts
+    # gene constraint scores
+    print("Loading DELs...")
     del <- read.table(del.tsv, header = TRUE, sep = "\t", comment.char = "")
     colnames(del)[1] <- "gene"
+    print("Loading DUPs...")
     dup <- read.table(dup.tsv, header = TRUE, sep = "\t", comment.char = "")
     colnames(dup)[1] <- "gene"
-    n.samples <- 62989
-    rcnv.scores <- read.table(rcnv.tsv, header = TRUE, sep = "\t", comment.char = "")
-    colnames(rcnv.scores)[1] <- "gene"
+    n.samples <- 63046
+    print("Loading constraint scores...")
+    constraint.scores <- read.table(constraint.tsv, header = TRUE, sep = "\t", comment.char = "")
+    colnames(constraint.scores)[1] <- "gene"
+    constraint.metric <- colnames(constraint.scores)[2]
+    print(constraint.metric)
 
+    print("Merging DEL/DUP data and constraint scores...")
     # Merge data by gene
     gene.scores <- merge(del, dup, by = "gene", all = FALSE, suffixes = c(".DEL", ".DUP"), sort = FALSE)
-    gene.scores <- merge(gene.scores, rcnv.scores, by = "gene", all = FALSE, sort = FALSE)
+    gene.scores <- merge(gene.scores, constraint.scores, by = "gene", all = FALSE, sort = FALSE)
+    print(
+        paste(
+            "Number of genes lost in merge of DSMap data and constraint scores:",
+            nrow(del) - nrow(gene.scores)
+        )
+    )
+    print(
+        paste(
+            "Number of genes with NA constraint score:", sum(is.na(gene.scores[, constraint.metric]))
+        )
+    )
+    gene.scores <- gene.scores[!is.na(gene.scores[, constraint.metric]), ]
 
-    # Calculate percentiles of pHaplo & pTriplo
-    gene.scores$bin.DEL <- ceiling(n.bins * rank(gene.scores$pHaplo) / nrow(gene.scores))
-    gene.scores$bin.DUP <- ceiling(n.bins * rank(gene.scores$pTriplo) / nrow(gene.scores))
+    print("Calculating bin quantiles...")
+    # Calculate bin quantiles of constraint score
+    gene.scores$bin <- ceiling(n.bins *
+        rank(gene.scores[, constraint.metric], na.last = "keep", ties.method = "random") /
+        nrow(gene.scores))
 
+    print("Computing DEL/DUP exp and O/E...")
     # Compute expected # of gene dels & dups given sample size
     gene.scores$exp.DEL <- (10^gene.scores$mu.DEL) * 2 * n.samples
     gene.scores$exp.DUP <- (10^gene.scores$mu.DUP) * 2 * n.samples
@@ -60,23 +77,23 @@ load.gene.scores <- function(del.tsv, dup.tsv, rcnv.tsv, n.bins = 100) {
     # Compute gene O/E
     gene.scores$oe.DEL <- gene.scores$n_svs.DEL / gene.scores$exp.DEL
     gene.scores$oe.DUP <- gene.scores$n_svs.DUP / gene.scores$exp.DUP
-    return(gene.scores)
+    return(list(gene.scores, constraint.metric))
 }
 
-# Compute gene O/E summary statistics with confidence intervals per rCNV score bin
+# Compute gene O/E summary statistics with confidence intervals per constraint score bin
 compute.oe.stats.by.bin <- function(gene.scores, n.bins = 100, n.bootstraps = 1000, seed = 42) {
-    oe.stats <- do.call("cbind", lapply(c("DEL", "DUP"), function(cnv) {
-        cbind(1:n.bins, do.call("rbind", lapply(1:n.bins, function(i) {
-            # Get obs and exp for genes in bin
-            idxs <- which(gene.scores[, paste("bin", cnv, sep = ".")] == i)
+    oe.stats <- cbind(1:n.bins, do.call("rbind", lapply(1:n.bins, function(i) {
+        # Get genes in bin
+        idxs <- which(gene.scores$bin == i)
+        # Bootstrap sampling of genes in bin
+        set.seed(seed)
+        bootstrap.idxs <- replicate(n.bootstraps, sample(idxs, length(idxs), replace = TRUE))
+
+        # Compute obs and exp values with bootstrap for DEL and DUP
+        do.call("cbind", lapply(c("DEL", "DUP"), function(cnv) {
+            # Get obs and exp for genes in bin and bootstrap samples
             obs <- gene.scores[idxs, paste("n_svs", cnv, sep = ".")]
             exp <- gene.scores[idxs, paste("exp", cnv, sep = ".")]
-
-            # Bootstrap sampling of genes
-            set.seed(seed)
-            bootstrap.idxs <- replicate(n.bootstraps, sample(idxs, length(idxs), replace = TRUE))
-
-            # Get obs and exp for genes in bootstrap samples
             bootstrap.obs <- matrix(
                 gene.scores[bootstrap.idxs, paste("n_svs", cnv, sep = ".")],
                 ncol = n.bootstraps
@@ -98,14 +115,14 @@ compute.oe.stats.by.bin <- function(gene.scores, n.bins = 100, n.bootstraps = 10
                 sum(obs) / sum(exp), sums.ci[1], sums.ci[2],
                 mad(obs / exp)
             )
-        })))
-    }))
-    colnames(oe.stats) <- do.call("c", lapply(c("DEL", "DUP"), function(cnv) {
+        }))
+    })))
+    colnames(oe.stats) <- c("bin", do.call("c", lapply(c("DEL", "DUP"), function(cnv) {
         paste(c(
-            "bin", "median", "median.lower", "median.upper",
+            "median", "median.lower", "median.upper",
             "sum", "sum.lower", "sum.upper", "mad"
         ), cnv, sep = ".")
-    }))
+    })))
     return(oe.stats)
 }
 
@@ -122,23 +139,21 @@ compute.cor <- function(oe.stats) {
     return(cors)
 }
 
-# Compute robust Z-scores of O/Es for genes within each rCNV score bin
+# Compute robust Z-scores of O/Es for genes within each constraint score bin
 compute.z.by.bin <- function(gene.scores, bin.stats) {
     zs.by.cnv <- lapply(c("DEL", "DUP"), function(cnv) {
         # Annotate genes with bin-level median and MAD O/E
         scores <- merge(
             gene.scores[, c(
-                "gene",
-                paste("bin", cnv, sep = "."),
+                "gene", "bin",
                 paste("n_svs", cnv, sep = "."),
                 paste("exp", cnv, sep = "."),
                 paste("oe", cnv, sep = ".")
             )], bin.stats[, c(
-                paste("bin", cnv, sep = "."),
+                "bin",
                 paste("median", cnv, sep = "."),
                 paste("mad", cnv, sep = ".")
-            )],
-            by = paste("bin", cnv, sep = ".")
+            )], "bin"
         )
         colnames(scores)[(ncol(scores) - 1):ncol(scores)] <- c(
             paste("bin.median", cnv, sep = "."),
@@ -158,8 +173,8 @@ compute.z.by.bin <- function(gene.scores, bin.stats) {
 ######################
 # Plotting functions #
 ######################
-# Plot O/Es of genes by their rCNV score bin
-plot.rcnv.bin.oe <- function(dat, cors) {
+# Plot O/Es of genes by their constraint score bin
+plot.constraint.bin.oe <- function(dat, cors, constraint.label) {
     par(mfrow = c(2, 2), mar = c(2, 3.5, 2, 1.5))
     plot(
         dat$sum.DEL,
@@ -178,7 +193,7 @@ plot.rcnv.bin.oe <- function(dat, cors) {
             "; p =", formatC(cors[cors$sum.type == "sum.DEL", "p"], format = "e", digits = 1)
         ), outer = FALSE, line = -1, cex.main = 1, font.main = 1
     )
-    mtext(1, text = "Genes Binned by pHaplo Percentile")
+    mtext(1, text = paste("Genes Binned by", constraint.label, "quantile"))
     mtext(2, line = 2.5, text = "Observed / Expected")
 
     plot(
@@ -198,7 +213,7 @@ plot.rcnv.bin.oe <- function(dat, cors) {
             "; p =", formatC(cors[cors$sum.type == "sum.DUP", "p"], format = "e", digits = 1)
         ), outer = FALSE, line = -1, cex.main = 1, font.main = 1
     )
-    mtext(1, text = "Genes Binned by pTriplo Percentile")
+    mtext(1, text = paste("Genes Binned by", constraint.label, "quantile"))
     mtext(2, line = 2.5, text = "Observed / Expected")
 
     plot(
@@ -218,7 +233,7 @@ plot.rcnv.bin.oe <- function(dat, cors) {
             "; p =", formatC(cors[cors$sum.type == "median.DEL", "p"], format = "e", digits = 1)
         ), outer = FALSE, line = -1, cex.main = 1, font.main = 1
     )
-    mtext(1, text = "Genes Binned by pHaplo Percentile")
+    mtext(1, text = paste("Genes Binned by", constraint.label, "quantile"))
     mtext(2, line = 2.5, text = "Observed / Expected")
 
     plot(
@@ -238,7 +253,7 @@ plot.rcnv.bin.oe <- function(dat, cors) {
             "; p =", formatC(cors[cors$sum.type == "median.DUP", "p"], format = "e", digits = 1)
         ), outer = FALSE, line = -1, cex.main = 1, font.main = 1
     )
-    mtext(1, text = "Genes Binned by pTriplo Percentile")
+    mtext(1, text = paste("Genes Binned by", constraint.label, "quantile"))
     mtext(2, line = 2.5, text = "Observed / Expected")
 }
 
@@ -248,12 +263,15 @@ plot.rcnv.bin.oe <- function(dat, cors) {
 ###########
 # List of command-line options
 option_list <- list(
+    make_option("--constraint-label",
+        type = "character",
+        help = "Output label for constraint metric if different from input TSV column name"
+    ),
     make_option("--n-bins",
         type = "integer", default = 100,
-        help = "Number of bins to divide genes into (default %default)."
+        help = "Number of bins to divide genes into [default %default]"
     )
 )
-# TODO: Parameterize CNV type, rCNV score type, bin quantile size?
 
 # Get command-line arguments & options
 arg_list <- c("del.tsv", "dup.tsv", "constraint.tsv", "out.pdf", "out_genes_del.tsv", "out_genes_dup.tsv")
@@ -279,29 +297,45 @@ if (length(args$args) != length(arg_list)) {
 # Writes args & opts to vars
 del.tsv <- args$args[1]
 dup.tsv <- args$args[2]
-rcnv.tsv <- args$args[3]
+constraint.tsv <- args$args[3]
 out.pdf <- args$args[4]
 out.genes.del.tsv <- args$args[5]
 out.genes.dup.tsv <- args$args[6]
 n.bins <- opts$`n-bins`
+constraint.label <- opts$`constraint-label`
+print(constraint.label)
 
-# Load gene obs, mus, exps, and rCNV scores
-gene.scores <- load.gene.scores(del.tsv, dup.tsv, rcnv.tsv, n.bins)
+print("Loading scores...")
+# Load gene obs, mus, exps, and constraint scores
+gene.scores <- load.gene.scores(del.tsv, dup.tsv, constraint.tsv, n.bins)
+constraint.metric <- gene.scores[[2]]
+print(constraint.metric)
+gene.scores <- gene.scores[[1]]
+print(head(gene.scores))
 
-# Compute gene O/E summary statistics per rCNV score bin
+print("Computing stats by bin...")
+# Compute gene O/E summary statistics per constraint score bin
 oe.stats <- compute.oe.stats.by.bin(gene.scores, n.bins)
+print(head(oe.stats))
 
-# Compute correlations for each O/E summary statistic type with rCNV score percentile
+print("Computing correlations...")
+# Compute correlations for each O/E summary statistic type with constraint score percentile
 cors <- compute.cor(oe.stats)
 
-# Plot O/Es of genes by their rCNV score bin
+print("Plotting...")
+# Plot O/Es of genes by their constraint score bin
 pdf(out.pdf, height = 5, width = 10)
-plot.rcnv.bin.oe(oe.stats, cors)
+plot.constraint.bin.oe(
+    oe.stats, cors,
+    ifelse(is.null(constraint.label), constraint.metric, constraint.label)
+)
 dev.off()
 
-# Compute robust Z-scores for O/Es of genes within rCNV score bins
+print("Computing robust Z-scores...")
+# Compute robust Z-scores for O/Es of genes within constraint score bins
 zs <- compute.z.by.bin(gene.scores, oe.stats)
 
+print("Writing out tables...")
 # Output genes ranked by robust Z-score
 write.table(
     zs[
