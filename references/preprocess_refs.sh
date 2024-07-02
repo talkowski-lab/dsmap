3adf4c3121c1f83d1718e1b7705b7dae2661c77f
lw453 <30123882+lw453@users.noreply.github.com>
2024-07-02 13:59:16 -0400

    Update centromere reference preprocessing

--- references/preprocess_refs.sh
+++ references/preprocess_refs.sh
@@ -14,20 +14,16 @@
 ################
 ### CENTROMERES
 ################
-
+# Download from UCSC browser track
 curl -O https://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/centromeres.txt.gz
 
-zcat centromeres.txt.gz \
+# Create BED of intervals with smallest and largest centromere coordinates per chromosome
+zcat < centromeres.txt.gz \
 | awk -F'\t' -v OFS='\t' '{print $2, $3, $4}' \
 | sort -Vk1,1 -k2,2n -k3,3n \
-| bgzip -c \
-> hg38.centromeres.all.bed.gz
-
-# Create BED of intervals with smallest and largest centromere coordinates per chromosome
-paste \
-    <(bedtools groupby -i hg38.centromeres.all.bed.gz -g 1 -c 2 -o min | cut -f1,2) \
-    <(bedtools groupby -i hg38.centromeres.all.bed.gz -g 1 -c 2 -o max | cut -f2) \
+| bedtools groupby -i hg38.centromeres.all.bed.gz -g 1 -c 2,2 -o min,max \
 | bgzip -c \
 > hg38.centromeres.bed.gz
 
+# Copy to GCP bucket
 gsutil cp hg38.centromeres.bed.gz gs://dsmap/data/references/
